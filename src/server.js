652b1946b77c067305d0d39b2c81998ae699d32d
Phạm Anh Quân <67911043+haiquanbg1@users.noreply.github.com>
2024-01-13 14:35:01 +0700

    create module

---
+++ src/server.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const configViewEngine = require("./config/viewEngine");
+
+const app = express();
+
+configViewEngine(app);
+
+const port = process.env.PORT;
+const host = process.env.HOST_NAME;
+
+app.get("/", (req, res) => {
+    res.render("home.ejs");
+})
+
+app.listen(port, host, () => {
+    console.log(`http://${host}:${port}`);
+})
